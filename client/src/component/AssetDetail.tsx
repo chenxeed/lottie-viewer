1c402110e49b95d9a540cc219bd83c76eb56ac05
Albert Mulia Shintra <albert.mulia@checkbox.ai>
2023-11-15 00:27:06 +0700

    (client) feat: download json file

--- client/src/component/AssetDetail.tsx
+++ client/src/component/AssetDetail.tsx
@@ -3,6 +3,7 @@ import { useStateSetViewAsset, useStateViewAsset } from '../store/assets';
 import clsx from 'clsx';
 import { Player, Controls, PlayerEvent } from '@lottiefiles/react-lottie-player';
 import { JsonViewer } from '@textea/json-viewer';
+import { downloadObjectAsJson } from '../helper/fileDownload';
 
 const KeyRenderer = ({ path }: any) => {
   return <div className='bg-slate-700 text-slate-200'>&quot;{path.slice(-1)}&quot;</div>;
@@ -26,6 +27,13 @@ export const AssetDetail = () => {
     setFrame(Math.round(controls.current?.props.instance.currentFrame));
   }
 
+  function downloadJson () {
+    if (!viewAsset) {
+      return
+    }
+    downloadObjectAsJson(viewAsset.file, viewAsset.title)
+  }
+
   return (
     <div
       className={clsx('fixed z-10 inset-0 overflow-hidden', viewAsset ? 'opacity-100 transition-all translate-y-0' : 'h-0 w-0 opacity-0 -translate-y-60')}
@@ -36,7 +44,7 @@ export const AssetDetail = () => {
           <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
             <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg lg:max-w-4xl">
               <div className="absolute top-2 right-2">
-                <button type="button" className="inline-flex justify-center rounded-md bg-stroke-n-600 px-3 py-2 text-sm font-semibold text-blue-400 shadow-sm hover:bg-stroke-linen-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" onClick={onClose}>
+                <button type="button" className="inline-flex justify-center rounded-md px-3 py-2 text-sm font-semibold shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" onClick={onClose}>
                   <span className="sr-only">Close</span>
                   <svg className="h-6 w-6 text-gray-400 hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" >
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
@@ -46,7 +54,13 @@ export const AssetDetail = () => {
               {viewAsset ? (
                 <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                   <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                    <h3 className="text-base font-semibold leading-6 text-gray-900" id="modal-title">{ viewAsset.title }</h3>
+                    <div className="flex items-center border-b-2 border-emerald-300 pb-2">
+                      <h3 className="text-base font-semibold leading-6 text-gray-900" id="modal-title">{ viewAsset.title }</h3>
+                      <button onClick={downloadJson} className="ml-4 bg-grey-light hover:bg-grey text-grey-darkest font-bold py-2 px-4 rounded inline-flex items-center shadow">
+                        <svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+                        <span className='ml-2 hidden sm:inline'>Download</span>
+                      </button>
+                    </div>
                     <div className='lg:flex'>
                       <div className="mt-2 w-full">
                         <Player style={{ height: 320 }} autoplay loop src={viewAsset.file} onEvent={onPlayerEvent}>
---
+++ client/src/helper/fileDownload.ts
@@ -0,0 +1,10 @@
+// Credit: https://stackoverflow.com/questions/19721439/download-json-object-as-a-file-from-browser
+export function downloadObjectAsJson(exportObj: Record<string, any>, exportName: string){
+  var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+  var downloadAnchorNode = document.createElement('a');
+  downloadAnchorNode.setAttribute("href", dataStr);
+  downloadAnchorNode.setAttribute("download", `${exportName}${exportName.endsWith('.json') ? '' : '.json'}`);
+  document.body.appendChild(downloadAnchorNode); // required for firefox
+  downloadAnchorNode.click();
+  downloadAnchorNode.remove();
+}
