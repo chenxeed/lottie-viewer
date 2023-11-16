85d71c76b07687b2f36b72c283e11c0546bc4d64
Albert Mulia Shintra <albert.mulia@checkbox.ai>
2023-11-16 15:27:14 +0700

    (client) feat: animate user login

--- client/src/component/CreateUserModal.tsx
+++ client/src/component/CreateUserModal.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useStateSetUser, useStateUser } from '../store/user';
 import { useSyncUser } from '../service/useSyncUser';
 import { Button } from '@mui/material';
+import { Player } from '@lottiefiles/react-lottie-player';
+import animationJSON from '../asset/arrow-down.json';
 
 export const CreateUserModal = () => {
   const [name, setName] = useState('');
@@ -39,12 +41,15 @@ export const CreateUserModal = () => {
           <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
             <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
               <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                <Player
+                  autoplay
+                  loop
+                  src={animationJSON}></Player>
                 <div className="sm:flex sm:items-start">
                   <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                     <h3 className="text-base font-semibold leading-6 text-gray-900" id="modal-title">Welcome!</h3>
                     <div className="mt-2">
-                      <p className="text-sm text-gray-500">We are glad you here to try out the Lottie Viewer. First, share with us your name to proceed.</p>                      
-                      <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">Username</label>
+                      <p className="text-sm text-gray-500">We are so glad that you here to try out the Lottie Viewer. First, share with us your name to proceed.</p>                      
                       <div className="mt-2">
                         <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                           <input type="text" name="username" id="username" autoComplete="username" className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Albert Mulia Shintra" onChange={onChangeUsername} />
