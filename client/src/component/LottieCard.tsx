4ac09276b600a85ece28c018215a5caafc55cd97
Albert Mulia Shintra <albert.mulia@checkbox.ai>
2023-11-18 19:29:37 +0700

    (client) style: improve hover transition

--- client/src/component/LottieCard.tsx
+++ client/src/component/LottieCard.tsx
@@ -14,7 +14,7 @@ export const LottieCard: FunctionComponent<
       className="card-shadow cursor-pointer flex flex-col justify-between bg-slate-50 hover:bg-teal-100 transition-colors duration-300 ease-in-out"
       onClick={onClick}
     >
-      <div className="w-full max-h-36 hover:max-h-40 lg:max-h-60 lg:hover:max-h-64 overflow-hidden relative">
+      <div className="w-full max-h-36 hover:scale-105 lg:max-h-60 lg:hover:scale-110 overflow-hidden relative transition-transform">
         {author && (
           <span className="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 absolute top-0 left-0">
             {author}
